4393e4298a2c48aef62df68f01da2bd2b47d6889
Soomin Lim <augustth511@gmail.com>
2022-08-22 22:25:00 +0900

    update register method add updateBoard method

--- src/main/java/hello/board/service/BoardService.java
+++ src/main/java/hello/board/service/BoardService.java
@@ -1,7 +1,9 @@
 package hello.board.service;
 
 import hello.board.entity.Board;
+import hello.board.entity.User;
 import hello.board.repository.BoardRepository;
+import hello.board.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,9 +17,16 @@ import java.util.Optional;
 public class BoardService {
 
     private final BoardRepository boardRepository;
+    private final UserRepository userRepository;
 
+    /**
+     * 게시글 생성
+     */
     @Transactional
-    public Long save(Board board) {
+    public Long register(String title, String content, Long userId) {
+        User user = userRepository.findOne(userId).orElseThrow();
+
+        Board board = Board.createBoard(title, content, user);
         boardRepository.save(board);
         return board.getId();
     }
@@ -37,8 +46,18 @@ public class BoardService {
     }
 
     /**
+     * 게시글 수정
+     */
+    @Transactional
+    public void updateBoard(Long id, String title, String content) {
+        Board board = boardRepository.findById(id).orElseThrow();
+        board.update(title, content);
+    }
+
+    /**
      * 게시글 삭제
      */
+    @Transactional
     public void deleteById(Long id) {
         boardRepository.deleteById(id);
     }

e7a94d6c782cf6a6f8ed2f2dae433bbfbca430d8
Soomin Lim <augustth511@gmail.com>
2022-08-24 11:40:59 +0900

    modify age type

--- src/main/java/hello/board/entity/User.java
+++ src/main/java/hello/board/entity/User.java
@@ -20,10 +20,10 @@ public class User {
     private String loginId; //로그인 ID
     private String password; //비밀번호
     private String name; //사용자 이름
-    private int age; //나이
+    private Integer age; //나이
 
     @Builder
-    public User(String loginId, String password, String name, int age) {
+    public User(String loginId, String password, String name, Integer age) {
         this.loginId = loginId;
         this.password = password;
         this.name = name;

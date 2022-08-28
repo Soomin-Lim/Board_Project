9fe68effe37ea937442de005ea59c831dfe53799
Soomin Lim <augustth511@gmail.com>
2022-08-28 22:54:43 +0900

    add login function

--- src/main/java/hello/board/controller/HomeController.java
+++ src/main/java/hello/board/controller/HomeController.java
@@ -1,16 +1,34 @@
 package hello.board.controller;
 
+import hello.board.entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 @Slf4j
 public class HomeController {
 
+//    @GetMapping("/")
+//    public String home() {
+//        log.info("home controller");
+//        return "home";
+//    }
+
     @GetMapping("/")
-    public String home() {
+    public String loginHome(@SessionAttribute(name = SessionConst.LOGIN_USER, required = false) User loginUser, Model model) {
+
         log.info("home controller");
-        return "home";
+
+        if (loginUser == null) {
+            log.info("main home");
+            return "home";
+        }
+
+        log.info("login home");
+        model.addAttribute("user", loginUser);
+        return "loginHome";
     }
 }
